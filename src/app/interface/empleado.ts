5a1bc271f5826f2f5a8e7709284cd416c4cdc244
Sam281055 <sl281055@gmail.com>
2024-08-01 12:53:18 -0400

    Auto-generated commit

--- src/app/interface/empleado.ts
+++
@@ -1,7 +0,0 @@
-export interface Empleado {
-    idEmpleado:number,
-    nombreCompleto:string,
-    correo:string,
-    sueldo:string,
-    fechaContrato:string
-}
--- src/app/interface/opcion.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface opciones {
-    nombre: string,
-    link: string
-  }
